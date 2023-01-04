a19a1bf1146e3ccc3628d41bb77bd13e65941f65
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2023-01-04 18:36:56 +0200

    base architecture

--- Cubit/rick_and_morty/macos/Flutter/GeneratedPluginRegistrant.swift
+++ Cubit/rick_and_morty/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus_macos
+import network_info_plus_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlugin"))
+  NetworkInfoPlusPlugin.register(with: registry.registrar(forPlugin: "NetworkInfoPlusPlugin"))
 }
