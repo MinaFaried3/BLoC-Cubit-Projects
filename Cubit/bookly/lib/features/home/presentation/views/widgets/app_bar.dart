4502f57fe53c39b655529158f17c84934f352a9e
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2023-04-22 22:35:35 +0200

    styles

--- Cubit/bookly/lib/features/home/presentation/views/widgets/app_bar.dart
+++ Cubit/bookly/lib/features/home/presentation/views/widgets/app_bar.dart
@@ -2,13 +2,19 @@ import 'package:bookly/core/utils/assets.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-AppBar buildMainAppBar(double height, double width) {
+AppBar buildMainAppBar(double height, double width, BuildContext context) {
   return AppBar(
     toolbarHeight: height / 13,
     automaticallyImplyLeading: false,
-    title: Image.asset(
-      AssetsData.logo,
-      width: width / 4,
+    leadingWidth: 0,
+    centerTitle: false,
+    titleSpacing: 0,
+    title: Padding(
+      padding: EdgeInsets.only(left: MediaQuery.of(context).size.width * 0.09),
+      child: Image.asset(
+        AssetsData.logo,
+        width: width / 4,
+      ),
     ),
     actions: [
       IconButton(
