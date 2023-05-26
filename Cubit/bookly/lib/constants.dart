f35ca695804a8d1ce5b868c9fdcc028babaaa091
Mina  Faried <mina.el.dihabi3.6.9@gmail.com>
2023-05-27 00:54:29 +0300

    project done

--- Cubit/bookly/lib/constants.dart
+++ Cubit/bookly/lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xff0c0b1f);
-const kTransitionDuration = Duration(milliseconds: 250);
+const kPrimaryColor = Color(0xff100B20);
+const kTranstionDuration = Duration(milliseconds: 250);
 const kGtSectraFine = 'GT Sectra Fine';
--- Cubit/bookly/lib/main.dart
+++ Cubit/bookly/lib/main.dart
@@ -1,40 +1,50 @@
+import 'package:bookly/Features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly/Features/home/presentation/manger/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly/Features/home/presentation/manger/newest_books_cubit/newset_books_cubit.dart';
 import 'package:bookly/constants.dart';
-import 'package:bookly/features/home/presentation/views/home_view.dart';
-import 'package:flutter/gestures.dart';
+import 'package:bookly/core/utils/api_service.dart';
+import 'package:bookly/core/utils/app_router.dart';
+import 'package:bookly/core/utils/service_locator.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:get/get.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:go_router/go_router.dart';
 import 'package:google_fonts/google_fonts.dart';
 
+import 'Features/Splash/presentation/views/splash_view.dart';
+
 void main() {
-  runApp(const BooklyApp());
+  setupServiceLocator();
+  runApp(const Bookly());
 }
 
-class BooklyApp extends StatelessWidget {
-  const BooklyApp({Key? key}) : super(key: key);
+class Bookly extends StatelessWidget {
+  const Bookly({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          )..fetchFeaturedBooks(),
+        ),
+        BlocProvider(
+          create: (context) => NewsetBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          )..fetchNewestBooks(),
+        )
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
           scaffoldBackgroundColor: kPrimaryColor,
-          appBarTheme: const AppBarTheme(
-              color: kPrimaryColor,
-              elevation: 0,
-              systemOverlayStyle:
-                  SystemUiOverlayStyle(statusBarColor: kPrimaryColor)),
           textTheme:
-              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme)),
-      scrollBehavior: const MaterialScrollBehavior().copyWith(
-        dragDevices: {
-          PointerDeviceKind.mouse,
-          PointerDeviceKind.touch,
-          PointerDeviceKind.stylus,
-          PointerDeviceKind.unknown,
-        },
+              GoogleFonts.montserratTextTheme(ThemeData.dark().textTheme),
+        ),
       ),
-      home: const HomeView(),
     );
   }
 }
